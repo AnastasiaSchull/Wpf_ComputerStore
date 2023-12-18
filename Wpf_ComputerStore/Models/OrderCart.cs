9834866c98661812bc941048bb7acdf25a81a127
Nastia <nastasia.schull@gmail.com>
2023-12-18 16:30:41 +0100

    #Task4 Перейменування назви класу OrderBucket на OrderCart.
    Перейменування назви класу OrderBucket на OrderCart.

--- Wpf_ComputerStore/Models/OrderCart.cs
+++ Wpf_ComputerStore/Models/OrderCart.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Wpf_ComputerStore.Models
 {
-    class OrderBucket
+    class OrderCart
     {
         public int ID { get; set; }
         public string CustomerName { get; set; }
