378e7895be3f2c0986a3f69b33a89c754d1cb8aa
Nastia <nastasia.schull@gmail.com>
2023-12-20 12:12:52 +0100

    #Task6 Додали клас PeripheralsType
    Додали клас PeripheralsType в папку Models

---
+++ Wpf_ComputerStore/Models/PeripheralsType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_ComputerStore.Models
+{
+    class PeripheralsType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual List<Peripherals> Peripheralss { get; set; }
+    }
+}
