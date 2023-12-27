246c961c3f70a3e19acda9b318a719d28f5544a0
Nastia <nastasia.schull@gmail.com>
2023-12-27 10:24:37 +0100

    #Task17 Змiни в DBContext
    Додали данi до ComputerDetail та поля до класу Computer

--- Wpf_ComputerStore/Models/Computer.cs
+++ Wpf_ComputerStore/Models/Computer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wpf_ComputerStore.Models;
 
 namespace Wpf_ComputerStore.Models
 {
@@ -10,9 +11,15 @@ namespace Wpf_ComputerStore.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public int Quantity { get; set; }
-        public virtual List<ComputerDetail> ComputerDetails { get; set; }
-
         public virtual ComputerType ComputerType { get; set; }
+        public virtual ComputerDetail RAM { get; set; }
+        public virtual ComputerDetail Motherboard { get; set; }
+        public virtual ComputerDetail CPU { get; set; }      
+        public virtual ComputerDetail HardDrive { get; set; }
+        public virtual ComputerDetail SSD { get; set; }
+        public virtual ComputerDetail VideoCard { get; set; }
+        public virtual ComputerDetail PowerSupply { get; set; }      
+        public int Quantity { get; set; }
+        public double Price { get; set; }
     }
 }
--- Wpf_ComputerStore/Models/ComputerDetail.cs
+++ Wpf_ComputerStore/Models/ComputerDetail.cs
@@ -14,5 +14,6 @@ namespace Wpf_ComputerStore.Models
         public int Quantity { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
+      
     }
 }
