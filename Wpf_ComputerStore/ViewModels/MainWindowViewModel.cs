e135c99e78dbba66b4244de4ce0b12aadf0c80a4
Nastia <nastasia.schull@gmail.com>
2023-12-27 14:57:16 +0100

    #Task22 Змiни у MainWindowViewModel та DBContext
    Додання поля та властивостi List<Computer> computersList.
    та реалiзацii getComputers(). У DBContext заповнення даними таблиць
    ComputerDetails та Computer

--- Wpf_ComputerStore/ViewModels/MainWindowViewModel.cs
+++ Wpf_ComputerStore/ViewModels/MainWindowViewModel.cs
@@ -12,6 +12,7 @@ namespace Wpf_ComputerStore.ViewModels
     {
         private List<ComputerDetail> computerDetailsList = new List<ComputerDetail>();
         private List<Category> categoriesList = new List<Category>();
+        private List<Computer> computersList = new List<Computer>();
 
         public List<ComputerDetail> ComputerDetailsList
         {
@@ -34,9 +35,19 @@ namespace Wpf_ComputerStore.ViewModels
             }
         }
 
+        public List<Computer> ComputersList
+        {
+            get { return computersList; }
+            set
+            {
+                computersList = value;
+                NotifyPropertyChanged("ComputersList");
+            }
+        }
+
         public MainWindowViewModel()
         {
-           
+            getComputers();
             getComputerDetails();
             getCategoriesList();
         }
@@ -63,6 +74,64 @@ namespace Wpf_ComputerStore.ViewModels
                 using (DBContext db = new DBContext())
                 {
                     CategoriesList = db.Categories.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void getComputers()
+        {
+            try
+            {
+                using (DBContext db = new DBContext())
+                {
+                    ComputersList = db.Computers.ToList();
+                    string rams = "";
+                    foreach(Computer computer in ComputersList) //костиль бо не працюе лiнива загрузка
+                    {
+                        rams += computer.RAM.Name;
+                        
+                    }
+                    string rams2 = "";
+                    foreach (Computer computer in ComputersList) 
+                    {
+                        rams2 += computer.Motherboard.Name;
+
+                    }
+                    string rams3 = "";
+                    foreach (Computer computer in ComputersList)
+                    {
+                        rams3 += computer.CPU.Name;
+
+                    }
+                    string rams4 = "";
+                    foreach (Computer computer in ComputersList)
+                    {
+                        rams4 += computer.HardDrive.Name;
+
+                    }
+                    string rams5 = "";
+                    foreach (Computer computer in ComputersList)
+                    {
+                        rams5 += computer.SDD.Name;
+
+                    }
+                    string rams6 = "";
+                    foreach (Computer computer in ComputersList)
+                    {
+                        rams6 += computer.VideoCard.Name;
+
+                    }
+                    string rams7 = "";
+                    foreach (Computer computer in ComputersList)
+                    {
+                        rams7 += computer.PowerSupply.Name;
+
+                    }
+
                 }
             }
             catch (Exception ex)
