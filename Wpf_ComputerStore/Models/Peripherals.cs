fc2e2547f75336b758731212a2f4a50987bf08c0
User_2 <chumachenko.bc@gmail.com>
2024-01-18 14:42:54 +0200

    #Task38 Дадання двох класів Item i ItemForSale, клас Item є батьківським для Computer, ComputerDetail i Peripherls. Також змінено клас OrderCart.

--- Wpf_ComputerStore/Models/Peripherals.cs
+++ Wpf_ComputerStore/Models/Peripherals.cs
@@ -6,13 +6,9 @@ using System.Threading.Tasks;
 
 namespace Wpf_ComputerStore.Models
 {
-    public class Peripherals
+    public class Peripherals:Item
     {
-        public int ID { get; set; }
-        public string? Name { get; set; }
         public virtual PeripheralsType PeripheralsType { get; set; }
-        public int Quantity { get; set; }
-        public double Price { get; set; }
         public string? Description { get; set; }
     }
 }
